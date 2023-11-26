09e2060f12f7e63f06f0f9c3d720a2dad7188946
yangzhenzhuozz <yangzhenzhuozz@126.com>
2023-11-27 00:16:02 +0800

    archive-1

--- js/NFA-1.ts
+++ js/NFA-1.ts
@@ -4,6 +4,7 @@ let slot = document.querySelector('#NFA-1') as HTMLDivElement;
 let root = slot.attachShadow({ mode: "open" });
 root.appendChild((slot.querySelector('template') as HTMLTemplateElement).content.cloneNode(true));//使style被隔离
 
+//注册箭头和state点击事件
 for (let item of (root.querySelectorAll('.arrow') as NodeListOf<SVGElement>)) {
     item.onclick = (e) => {
         arrowMotion(e.currentTarget as SVGElement, 300);
@@ -35,161 +36,154 @@ function DarkTools(ids: string[]) {
         dark(root.querySelector(`#${id}`) as SVGAElement);
     }
 }
-
-(root.querySelector('button[id=aab]') as HTMLButtonElement).onclick = async () => {
-    let dur = 500;
-    let Multiplying = 4;
-    let now = root.querySelector('#now') as HTMLSpanElement;
-    let letters = root.querySelector('#letters') as HTMLSpanElement;
-    let letter = root.querySelector('#letter') as HTMLSpanElement;
-
-    //start
-    arrowmotionTools(['p_0_0'], dur);
-    await delay(dur);
-    LightTools(['state0']);
-    await delay(dur);
-    arrowmotionTools(['p_0_1', 'p_0_3'], dur);
-    await delay(dur);
-    LightTools(['state1', 'state3']);
-    await delay(dur);
-    arrowmotionTools(['p_3_4'], dur);
-    await delay(dur);
-    LightTools(['state4']);
-    now.innerText = ['start', 1, 3, 4].join();
-    letters.innerText = ['a', 'b'].join();
-    now.style.border = '1px solid red';
-    letters.style.border = '1px solid red';
-    await delay(dur * Multiplying);
+function drakAll() {
     DarkTools([
         'p_0_0',
         'p_0_1',
         'p_0_3',
+        'p_1_2',
+        'p_2_1',
+        'p_2_3',
         'p_3_4',
+        'p_4_5',
         'state0',
         'state1',
-        'state3',
-        'state4'
-    ]);
-
-    //a
-    letter.innerText = 'a(1)';
-    arrowmotionTools(['p_1_2'], dur);
-    await delay(dur);
-    LightTools(['state2']);
-    await delay(dur);
-    arrowmotionTools(['p_2_1', 'p_2_3'], dur);
-    await delay(dur);
-    LightTools(['state1', 'state3']);
-    await delay(dur);
-    arrowmotionTools(['p_3_4'], dur);
-    await delay(dur);
-    LightTools(['state4']);
-    now.innerText = [1, 2, 3, 4].join();
-    await delay(dur * Multiplying);
-    DarkTools([
-        'p_1_2',
-        'p_2_1',
-        'p_2_3',
-        'p_3_4',
-        'state1',
         'state2',
         'state3',
-        'state4'
-    ]);
-
-    //a
-    letter.innerText = 'a(2)';
-    arrowmotionTools(['p_1_2'], dur);
-    await delay(dur);
-    LightTools(['state2']);
-    await delay(dur);
-    arrowmotionTools(['p_2_1', 'p_2_3'], dur);
-    await delay(dur);
-    LightTools(['state1', 'state3']);
-    await delay(dur);
-    arrowmotionTools(['p_3_4'], dur);
-    await delay(dur);
-    LightTools(['state4']);
-    now.innerText = [1, 2, 3, 4].join();
-    await delay(dur * Multiplying);
-    DarkTools([
-        'p_1_2',
-        'p_2_1',
-        'p_2_3',
-        'p_3_4',
-        'state1',
-        'state2',
-        'state3',
-        'state4'
-    ]);
-
-    //b
-    letter.innerText = 'b';
-    arrowmotionTools(['p_4_5'], dur);
-    await delay(dur);
-    LightTools(['state5']);
-    now.innerText = ['end'].join();
-    letters.innerText = [].join();
-    await delay(dur * Multiplying);
-    DarkTools([
-        'p_4_5',
+        'state4',
         'state5'
     ]);
+}
 
-    //end
-    letters.style.border='';
-    letter.innerText = '';
+class Automaton_aab {
+    private dur = 500;
+    private Multiplying = 4;
+    private now = root.querySelector('#now') as HTMLSpanElement;
+    private letters = root.querySelector('#letters') as HTMLSpanElement;
+    private letter = root.querySelector('#letter') as HTMLSpanElement;
+    private status = -1;
+    private async to_start() {
+        this.letter.innerText = `goto start`;
+        drakAll();
+        arrowmotionTools(['p_0_0'], this.dur);
+        await delay(this.dur);
+        LightTools(['state0']);
+        await delay(this.dur);
+        arrowmotionTools(['p_0_1', 'p_0_3'], this.dur);
+        await delay(this.dur);
+        LightTools(['state1', 'state3']);
+        await delay(this.dur);
+        arrowmotionTools(['p_3_4'], this.dur);
+        await delay(this.dur);
+        LightTools(['state4']);
+        this.now.innerText = ['start', 1, 3, 4].join();
+        this.letters.innerText = ['a', 'b'].join();
+        this.now.style.border = '1px solid red';
+        this.letters.style.border = '1px solid red';
+    };
+    private async accept_a() {
+        drakAll();
+        this.letter.innerText = `a(${this.status + 1})`;
+        arrowmotionTools(['p_1_2'], this.dur);
+        await delay(this.dur);
+        LightTools(['state2']);
+        await delay(this.dur);
+        arrowmotionTools(['p_2_1', 'p_2_3'], this.dur);
+        await delay(this.dur);
+        LightTools(['state1', 'state3']);
+        await delay(this.dur);
+        arrowmotionTools(['p_3_4'], this.dur);
+        await delay(this.dur);
+        LightTools(['state4']);
+        this.now.innerText = [1, 2, 3, 4].join();
+    }
+    private async accept_b() {
+        drakAll();
+        this.letter.innerText = 'b';
+        arrowmotionTools(['p_4_5'], this.dur);
+        await delay(this.dur);
+        LightTools(['state5']);
+        this.now.innerText = ['end'].join();
+        this.letters.innerText = [].join();
+        this.letters.style.border = '';
+    }
+    public run() {
+        console.log(this.status);
+        switch (this.status) {
+            case -1:
+                this.to_start();
+                this.status++;
+                break;
+            case 0:
+                this.accept_a();
+                this.status++;
+                break;
+            case 1:
+                this.accept_a();
+                this.status++;
+                break;
+            case 2:
+                this.accept_b();
+                this.status = -1;
+                break;
+        }
+    }
+}
+class Automaton_b {
+    private dur = 500;
+    private Multiplying = 4;
+    private now = root.querySelector('#now') as HTMLSpanElement;
+    private letters = root.querySelector('#letters') as HTMLSpanElement;
+    private letter = root.querySelector('#letter') as HTMLSpanElement;
+    private status = -1;
+    private async to_start() {
+        this.letter.innerText = `goto start`;
+        drakAll();
+        arrowmotionTools(['p_0_0'], this.dur);
+        await delay(this.dur);
+        LightTools(['state0']);
+        await delay(this.dur);
+        arrowmotionTools(['p_0_1', 'p_0_3'], this.dur);
+        await delay(this.dur);
+        LightTools(['state1', 'state3']);
+        await delay(this.dur);
+        arrowmotionTools(['p_3_4'], this.dur);
+        await delay(this.dur);
+        LightTools(['state4']);
+        this.now.innerText = ['start', 1, 3, 4].join();
+        this.letters.innerText = ['a', 'b'].join();
+        this.now.style.border = '1px solid red';
+        this.letters.style.border = '1px solid red';
+    };
+    private async accept_b() {
+        drakAll();
+        this.letter.innerText = 'b';
+        arrowmotionTools(['p_4_5'], this.dur);
+        await delay(this.dur);
+        LightTools(['state5']);
+        this.now.innerText = ['end'].join();
+        this.letters.innerText = [].join();
+        this.letters.style.border = '';
+    }
+    public run() {
+        console.log(this.status);
+        switch (this.status) {
+            case -1:
+                this.to_start();
+                this.status++;
+                break;
+            case 0:
+                this.accept_b();
+                this.status = -1;
+                break;
+        }
+    }
+}
+let automaton_aab = new Automaton_aab();
+let automaton_b = new Automaton_b();
+(root.querySelector('button[id=aab]') as HTMLButtonElement).onclick = () => {
+    automaton_aab.run();
 };
-
-(root.querySelector('button[id=b]') as HTMLButtonElement).onclick = async () => {
-    let dur = 500;
-    let Multiplying = 4;
-    let now = root.querySelector('#now') as HTMLSpanElement;
-    let letters = root.querySelector('#letters') as HTMLSpanElement;
-    let letter = root.querySelector('#letter') as HTMLSpanElement;
-
-    //start
-    arrowmotionTools(['p_0_0'], dur);
-    await delay(dur);
-    LightTools(['state0']);
-    await delay(dur);
-    arrowmotionTools(['p_0_1', 'p_0_3'], dur);
-    await delay(dur);
-    LightTools(['state1', 'state3']);
-    await delay(dur);
-    arrowmotionTools(['p_3_4'], dur);
-    await delay(dur);
-    LightTools(['state4']);
-    now.innerText = ['start', 1, 3, 4].join();
-    letters.innerText = ['a', 'b'].join();
-    now.style.border = '1px solid red';
-    letters.style.border = '1px solid red';
-    await delay(dur * Multiplying);
-    DarkTools([
-        'p_0_0',
-        'p_0_1',
-        'p_0_3',
-        'p_3_4',
-        'state0',
-        'state1',
-        'state3',
-        'state4'
-    ]);
-    
-    //b
-    letter.innerText = 'b';
-    arrowmotionTools(['p_4_5'], dur);
-    await delay(dur);
-    LightTools(['state5']);
-    now.innerText = ['end'].join();
-    letters.innerText = [].join();
-    await delay(dur * Multiplying);
-    DarkTools([
-        'p_4_5',
-        'state5'
-    ]);
-
-    //end
-    letters.style.border='';
-    letter.innerText = '';
+(root.querySelector('button[id=b]') as HTMLButtonElement).onclick = () => {
+    automaton_b.run();
 };
